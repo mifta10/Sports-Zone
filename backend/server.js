53a0125b70caa57282abbf67c0e12b592fa7898f
mifta10 <miftauddinkhan10@gmail.com>
2020-12-20 00:40:10 +0600

    atlas connection

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://mifta10:mifta@cluster0.8pac6.mongodb.net/sports-zone?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mifta10:mifta@cluster0.8pac6.mongodb.net/sports-zone?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
